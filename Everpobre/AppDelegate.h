88b49fc1f83ad34d9febbf7b35036b465db6d495
Yosamac <yosamac@gmail.com>
2015-02-02 12:05:23 +0100

    Initial Commit

---
+++ Everpobre/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Everpobre
+//
+//  Created by Yosnier on 02/02/15.
+//  Copyright (c) 2015 YOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
